b6f20e23ed54c436a8a3d994a09cd4cf345140dc
wangdeng <wang0379deng>
2022-02-16 09:15:04 +0800

    0.007

---
+++ src/main/java/com/wangdeng/fastermc/intf/IClumpedOrb.java
@@ -0,0 +1,11 @@
+package com.wangdeng.fastermc.intf;
+
+import java.util.Map;
+
+public interface IClumpedOrb {
+    
+    public Map<Integer, Integer> clumps$getClumpedMap();
+    
+    public void clumps$setClumpedMap(Map<Integer, Integer> map);
+    
+}
